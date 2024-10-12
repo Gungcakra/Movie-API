6347947ac8c07dec0aa061dbf65479e3987b209f
Gungcakra <agungcakra888@gmail.com>
2024-10-12 13:27:41 +0800

    tes header

--- api/server.js
+++ api/server.js
@@ -87,7 +87,10 @@ app.get('/api/movie-horror', async (req, res) => {
     try {
         const { data } = await axios.get(url, {
             headers: {
-                'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/90.0.4430.212 Safari/537.36'
+                'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/104.0.5112.81 Safari/537.36',
+                'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8',
+                'Accept-Language': 'en-US,en;q=0.5',
+                'Connection': 'keep-alive'
             }
         });
         const $ = cheerio.load(data);
